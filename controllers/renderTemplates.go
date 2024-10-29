29b6f584c86457addf931fda7dd26144ad5f3066
Youssef El Asri <168789623+yelasri07@users.noreply.github.com>
2024-10-29 17:22:04 +0100

    Add files via upload

--- controllers/renderTemplates.go
+++ controllers/renderTemplates.go
@@ -6,13 +6,12 @@ import (
 )
 
 func RenderTempalte(w http.ResponseWriter, url string, data any, status int) error {
-	w.WriteHeader(status)
 	tmpl, err := template.ParseFiles(url)
 	if err != nil {
 		return err
 	}
 
-	
+	w.WriteHeader(status)
 	err = tmpl.Execute(w, data)
 	if err != nil {
 		return err
